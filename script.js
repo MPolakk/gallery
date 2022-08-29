5d8b3ef7843f71b40cea95140d1e71f6053ea8aa
Marcin Polak <m_polak98@o2.pl>
2022-08-29 21:32:50 +0200

    Add keyframes for gallery slider

---
+++ script.js
@@ -0,0 +1,26 @@
+const prevButton = document.querySelector('.gallery__prev');
+const nextButton = document.querySelector('.gallery__next');
+const prevPhoto = document.querySelector('.gallery__prevphoto');
+const activePhoto = document.querySelector('.gallery__activephoto');
+const nextPhoto = document.querySelector('.gallery__nextphoto');
+const photoNumber = document.querySelector('.gallery__photonumber');
+
+const listOfPhotos = [
+{src: 'img1'},
+{src: 'img2'},
+{src: 'img3'}
+]
+const changePhotoNumber = () => {
+
+
+}
+const changePhotoPrev = () => {
+ 
+}
+const changePhotoNext = () => {
+
+}
+
+
+prevButton.addEventListener('click', changePhotoPrev);
+nextButton.addEventListener('click', changePhotoNext);
