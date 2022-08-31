6a543f1eb74c64a763c6cf9b9d2b59ab3b976a3c
Marcin Polak <m_polak98@o2.pl>
2022-08-31 19:52:54 +0200

    Add animation and functions to next/prev buttons

--- script.js
+++ script.js
@@ -6,21 +6,59 @@ const nextPhoto = document.querySelector('.gallery__nextphoto');
 const photoNumber = document.querySelector('.gallery__photonumber');
 
 const listOfPhotos = [
-{src: 'img1'},
-{src: 'img2'},
-{src: 'img3'}
+{src: 'img_1'},
+{src: 'img_2'},
+{src: 'img_3'},
+{src: 'img_4'}
+
 ]
-const changePhotoNumber = () => {
+let activePhotoNum = 0;
+let animationEnd = true;
 
 
-}
+
 const changePhotoPrev = () => {
- 
+  if (animationEnd) {
+  photoAnimation()
+ if (activePhotoNum == 0) return;
+ else {
+  
+  activePhotoNum--
+
+ }
+  updateAttributes()
+}
+
 }
 const changePhotoNext = () => {
+  console.log("Działa")
+  if (animationEnd) {
+  photoAnimation()
+  if (activePhotoNum == listOfPhotos.length - 1) {return}
+  else {
+
+   activePhotoNum++
+  
+  }
+  updateAttributes()
+}
+}
+const updateAttributes = () => {
+
+ activePhoto.setAttribute('src', `/assets/imgs/${listOfPhotos[activePhotoNum].src}.jpg`)
 
 }
 
+const photoAnimation = () => {
+  animationEnd = false;
+  activePhoto.classList.add('animationphoto');
+  
+  
+}
 
+activePhoto.addEventListener('animationend', () => {
+  activePhoto.classList.remove('animationphoto');
+  animationEnd = true;
+})
 prevButton.addEventListener('click', changePhotoPrev);
 nextButton.addEventListener('click', changePhotoNext);
