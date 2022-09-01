95ba71962cb426ad97a3f1889a46c87a4f00b762
Marcin Polak <m_polak98@o2.pl>
2022-09-01 19:52:55 +0200

    Add animation block at first and last photo. Change styles to more friendly for eyes. Add display flex to counter and next/prev button

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
 const prevButton = document.querySelector('.gallery__prev');
 const nextButton = document.querySelector('.gallery__next');
-const prevPhoto = document.querySelector('.gallery__prevphoto');
 const activePhoto = document.querySelector('.gallery__activephoto');
-const nextPhoto = document.querySelector('.gallery__nextphoto');
 const photoNumber = document.querySelector('.gallery__photonumber');
 
 const listOfPhotos = [
 {src: 'img_1'},
 {src: 'img_2'},
 {src: 'img_3'},
-{src: 'img_4'}
+{src: 'img_4'},
+{src: 'img_5'}
 
 ]
 let activePhotoNum = 0;
 let animationEnd = true;
 
+photoNumber.innerHTML = `${activePhotoNum+1}/${listOfPhotos.length}`
+
 
 
 const changePhotoPrev = () => {
+  if (activePhotoNum == 0) return
+  else {
   if (animationEnd) {
   photoAnimation()
- if (activePhotoNum == 0) return;
- else {
+ 
   
   activePhotoNum--
 
- }
+
   updateAttributes()
+}
 }
 
 }
 const changePhotoNext = () => {
-  console.log("Działa")
+  if(activePhotoNum == listOfPhotos.length - 1) {return}
+  else {
   if (animationEnd) {
   photoAnimation()
-  if (activePhotoNum == listOfPhotos.length - 1) {return}
-  else {
-
+  
    activePhotoNum++
   
-  }
   updateAttributes()
-}
+}}
 }
 const updateAttributes = () => {
-
+  updatePhotoNumber()
  activePhoto.setAttribute('src', `/assets/imgs/${listOfPhotos[activePhotoNum].src}.jpg`)
 
 }
@@ -54,6 +55,11 @@ const photoAnimation = () => {
   activePhoto.classList.add('animationphoto');
   
   
+}
+
+const updatePhotoNumber = () => {
+  console.log("dziala")
+  photoNumber.innerHTML = `${activePhotoNum+1}/${listOfPhotos.length}`
 }
 
 activePhoto.addEventListener('animationend', () => {
