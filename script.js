e0944dae9f2ff5f36b97a6e4df2b447de4658758
Marcin Polak <m_polak98@o2.pl>
2022-09-14 22:36:42 +0200

    Add changing delay by slider and change buttons position

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Gallery
-
+const container = document.querySelector('.container');
 const prevButton = document.querySelector('.gallery__prev');
 const nextButton = document.querySelector('.gallery__next');
 const activePhoto = document.querySelector('.gallery__activephoto');
@@ -17,12 +17,15 @@ const listOfPhotos = [
 ]
 let activePhotoNum = 0;
 let animationEnd = true;
+let imageWidth;
 
+let photoWidth;
 photoNumber.innerHTML = `${activePhotoNum+1}/${listOfPhotos.length}`
 
 
 
 const changePhotoPrev = () => {
+  
   if (activePhotoNum == 0) {
     activePhotoNum = listOfPhotos.length - 1; 
     photoAnimation();
@@ -38,10 +41,12 @@ const changePhotoPrev = () => {
 
   updateAttributes()
 }
+
 }
 
 }
-const changePhotoNext = () => {
+const changePhotoNext = (e) => {
+  
   if(activePhotoNum == listOfPhotos.length - 1) {
     activePhotoNum = 0; 
     photoAnimation();
@@ -51,39 +56,54 @@ const changePhotoNext = () => {
   if (animationEnd) {
   photoAnimation()
   
-   activePhotoNum++
-  
+  activePhotoNum++
+   
   updateAttributes()
 }}
+
 }
 const updateAttributes = () => {
   updatePhotoNumber()
- activePhoto.setAttribute('src', `/assets/imgs/${listOfPhotos[activePhotoNum].src}.jpg`)
-
+  activePhoto.setAttribute('src', `/assets/imgs/${listOfPhotos[activePhotoNum].src}.jpg`)
+  
 }
 
 const photoAnimation = () => {
   animationEnd = false;
   activePhoto.classList.add('animationphoto');
   
-  
 }
 
-const updatePhotoNumber = () => {
-  photoNumber.innerHTML = `${activePhotoNum+1}/${listOfPhotos.length}`
-}
+
 
 activePhoto.addEventListener('animationend', () => {
   activePhoto.classList.remove('animationphoto');
   animationEnd = true;
+  
 })
+const updatePhotoNumber = () => {
+  photoNumber.innerHTML = `${activePhotoNum+1}/${listOfPhotos.length}`
+  
+}
+const changeButtonsPosition = (e) => {
+  imageWidth = activePhoto.clientWidth;
+  containerWidth = container.clientWidth;
+  console.log(((containerWidth - imageWidth) / 2) - ((containerWidth - imageWidth) / 2))
+  prevButton.style.left = (containerWidth - imageWidth) / 2 - 150  + "px"
+  nextButton.style.right = (containerWidth - imageWidth) / 2 - 150 + "px"
 
+
+}
+
+updateAttributes();
 prevButton.addEventListener('click', changePhotoPrev);
 nextButton.addEventListener('click', changePhotoNext);
-
+activePhoto.addEventListener('load', changeButtonsPosition)
 // AutoPlay
 
 const autoPlayButton = document.querySelector('.gallery__auto')
+
+let autoPlayDelay;
 
 let autoPlayID;
 
@@ -100,13 +120,51 @@ const autoPlayTurnOn = () => {
   autoPlayActive = !autoPlayActive;
   
   if(autoPlayActive) {
-    autoPlayID = setInterval(autoPlay, 5000)
+    autoPlayID = setInterval(autoPlay, autoPlayDelay)
   } else {
     clearInterval(autoPlayID)
   }
-
+  autoPlayButton.classList.toggle('gallery__autoOn')
+  
 }
 autoPlayButton.addEventListener('click', autoPlayTurnOn)
+
+
+
+//AutoPlay Time
+
+const range = document.querySelector('.autoplay__time')
+const timeValue = document.querySelector('.autoplay__timevalue')
+
+
+const changeRange = (e) => {
+  autoPlayDelay = e.target.value * 1000;
+  
+  if(autoPlayActive) {
+    clearInterval(autoPlayID);
+    autoPlayID = setInterval(autoPlay, autoPlayDelay);
+  }
+  
+  timeValue.innerText = e.target.value + "s";
+}
+
+
+
+range.addEventListener('mouseup', changeRange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Menu
 
 const menuList = document.querySelector('.menu__list');
